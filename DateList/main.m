074f3eb80e8475d9f5bd782d891d05a215f3302a
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-06 12:42:11 -0500

    refactor: use old-style syntax for nsarray initialization and indexing

--- DateList/main.m
+++ DateList/main.m
@@ -14,16 +14,19 @@ int main(int argc, const char * argv[]) {
         NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
         
 //        NSMutableArray *dateList = [[NSMutableArray alloc] init];
-        NSMutableArray *dateList = [NSMutableArray array];
+//        NSMutableArray *dateList = [NSMutableArray array];
+        NSArray *dateList = [NSArray arrayWithObjects:now, tomorrow, yesterday, nil];
         
-        [dateList addObject:now];
-        [dateList addObject:tomorrow];
+//        [dateList addObject:now];
+//        [dateList addObject:tomorrow];
         
-        [dateList insertObject:yesterday atIndex:0];
+//        [dateList insertObject:yesterday atIndex:0];
         
-        NSLog(@"The first date is %@", dateList[0]);
-        NSLog(@"The third date is %@", dateList[2]);
+//        NSLog(@"The first date is %@", dateList[0]);
+//        NSLog(@"The third date is %@", dateList[2]);
 //        NSLog(@"The fourth date is %@", dateList[3]); // out-of-range error
+        NSLog(@"The first date is %@", [dateList objectAtIndex:0]);
+        NSLog(@"The third date is %@", [dateList objectAtIndex:2]);
         
         NSInteger dateCount = [dateList count];
         
@@ -33,8 +36,8 @@ int main(int argc, const char * argv[]) {
             NSLog(@"Here is another date: %@", d);
         }
         
-        NSLog(@"Removing an item...");
-        [dateList removeObjectAtIndex:0];
+//        NSLog(@"Removing an item...");
+//        [dateList removeObjectAtIndex:0];
         
         NSLog(@"Now the first date is: %@", dateList[0]);
     }
