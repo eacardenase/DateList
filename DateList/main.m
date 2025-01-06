5f7dc22cc7071773f55f9bc00fa0cfa9befd94a0
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-06 12:17:15 -0500

    feat: use mutable array for crud operations on arrays

--- DateList/main.m
+++ DateList/main.m
@@ -13,7 +13,13 @@ int main(int argc, const char * argv[]) {
         NSDate *tomorrow = [now dateByAddingTimeInterval:24.0 * 60.0 * 60.0];
         NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
         
-        NSArray *dateList = @[now, tomorrow, yesterday];
+//        NSMutableArray *dateList = [[NSMutableArray alloc] init];
+        NSMutableArray *dateList = [NSMutableArray array];
+        
+        [dateList addObject:now];
+        [dateList addObject:tomorrow];
+        
+        [dateList insertObject:yesterday atIndex:0];
         
         NSLog(@"The first date is %@", dateList[0]);
         NSLog(@"The third date is %@", dateList[2]);
@@ -26,6 +32,11 @@ int main(int argc, const char * argv[]) {
         for (NSDate *d in dateList) {
             NSLog(@"Here is another date: %@", d);
         }
+        
+        NSLog(@"Removing an item...");
+        [dateList removeObjectAtIndex:0];
+        
+        NSLog(@"Now the first date is: %@", dateList[0]);
     }
     return 0;
 }
