08bb052cc53896dac8190088a7d010f013cd525f
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-06 11:42:48 -0500

    feat: access items in the array by index

--- DateList/main.m
+++ DateList/main.m
@@ -14,6 +14,12 @@ int main(int argc, const char * argv[]) {
         NSDate *yesterday = [now dateByAddingTimeInterval:-24.0 * 60.0 * 60.0];
         
         NSArray *dateList = @[now, tomorrow, yesterday];
+        
+        NSLog(@"The first date is %@", dateList[0]);
+        NSLog(@"The third date is %@", dateList[2]);
+//        NSLog(@"The fourth date is %@", dateList[3]); // out-of-range error
+        
+        NSLog(@"How many items are inside dateList? There are %lu dates", [dateList count]);
     }
     return 0;
 }
